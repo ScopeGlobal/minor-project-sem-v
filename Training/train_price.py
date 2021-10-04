fcd3b6b781cf61019a699cd3b715df41f70e4e8e
ScopeGlobal <sridhar.m@somaiya.edu>
2021-10-04 14:24:46 +0530

    Added function to use pycoingecko to get historical data

--- Training/train_price.py
+++ Training/train_price.py
@@ -7,7 +7,6 @@ import os
 import matplotlib.pyplot as plt 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error
 import pycoingecko
 import pickle
 
@@ -15,10 +14,9 @@ filename = 'finalized.sav'
 
 def train_price(currency):
    df = pd.read_csv('./data/archive/coin_' + currency + '.csv')
-
    
    # taking the features we are interested in and creating a new dataframe
-   interested_features = ["Open", "Close", "High", "Low", "Marketcap"]  #TODO: Try and code date as well
+   interested_features = ["Open", "Close", "High", "Low", "Marketcap"]
    cleaned_df = df[interested_features]
 
    # now, out of these interests, we select the features we think will influence the values
@@ -34,23 +32,38 @@ def train_price(currency):
    
    # now, we create our Random Forests Model.
    # TODO: Add parameters to prevent overfitting.
-   rf_model = RandomForestRegressor()
+   rf_model = RandomForestRegressor(n_estimators=500, max_features=3, min_samples_leaf=50)
    rf_model.fit(xtrain, ytrain.values.ravel())
    
    # predicting the values 
-   rf_prediction = rf_model.predict(xval)  # Model complete
+   rf_prediction = rf_model.predict(xval)
    if not os.path.isfile(filename):
     pickle.dump(rf_model, open(filename, 'wb'))
 
-   # testing to see if our values are accurate
-    print(rf_model.predict(x.tail()))
 
-    print("Actual Values")
-    print(y)
+def real_time_test(currency):
+    cg = pycoingecko.CoinGeckoAPI()
+    real_time_df = pd.DataFrame(
+        cg.get_coin_ohlc_by_id(id=currency.lower(), days=30, vs_currency='usd'), 
+        columns=['Marketcap', 'Open', 'High', 'Low', 'Close']
+    )
+    
 
-    # using mean absolute error to further determine accuracy
-    rf_values_error = mean_absolute_error(yval, rf_prediction)
-    print(rf_values_error)
+    cleaned_real_time_df = real_time_df[["High", "Low", "Marketcap"]]
+    real_time_result = real_time_df["Close"]
+
+    if not os.path.isfile('real_time_result_rf'):
+        real_time_result.to_csv('real_time_result_rf.csv')
+
+    load_model = pickle.load(open(filename, 'rb'))
+    result = load_model.predict(cleaned_real_time_df)
+    print(result)
+
+    if not os.path.isfile('predicted_result_rf'):
+        predicted_df = pd.DataFrame(result)
+        predicted_df.to_csv('predicted_result_rf.csv')
+         
 
 
 train_price('Bitcoin')
+real_time_test('Bitcoin') 
