cfe1871d18da24a28a964c033bc9b2014a05d449
Kri-hika <73770973+Kri-hika@users.noreply.github.com>
2021-11-12 02:33:38 +0530

    charts updated for Bitcoin ,Ethereum, BinanceCoin and Tether in coin-info.html

--- Crypto_prediction/apps/views.py
+++ Crypto_prediction/apps/views.py
@@ -31,6 +31,7 @@ def index(request):
     
     return render(request, 'index.html', {"data" : data})
 
+
 def coin_info(request):
     url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false'
     data = requests.get(url).json()
